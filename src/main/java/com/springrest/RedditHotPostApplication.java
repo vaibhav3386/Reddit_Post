5cf903f419c0cce53dcc735ee4b0e87d343a2570
vaibhav <vaibhavmehsana123@gmail.com>
2023-07-11 14:19:17 +0530

    11 Jul

--- src/main/java/com/springrest/RedditHotPostApplication.java
+++ src/main/java/com/springrest/RedditHotPostApplication.java
@@ -2,11 +2,8 @@ package com.springrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
 
 @SpringBootApplication
-@EnableScheduling
 public class RedditHotPostApplication {
     public static void main(String[] args) {
         SpringApplication.run(RedditHotPostApplication.class, args);
--- src/main/java/com/springrest/controller/RedditServiceController.java
+++ src/main/java/com/springrest/controller/RedditServiceController.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.springrest.model.RedditPostStructure;
 import com.springrest.service.HotPostRedditService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
